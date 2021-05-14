713183183ab1813af0c3197ca9a33822c891b78b
HDLim <84057877+HDLim@users.noreply.github.com>
2021-05-14 21:44:19 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -13,8 +13,10 @@ public class Client {
 		BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
 		
 		try (Socket sock = new Socket("127.0.0.1",6013)){
+			
 			PrintWriter cout = new PrintWriter(sock.getOutputStream(),true);
 			BufferedReader cin = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			
 			String line=null;
 			System.out.println("Connection to server Successful...");
 			
@@ -22,6 +24,7 @@ public class Client {
 				System.out.print("Enter Text: ");
 				line = scan.readLine();
 				cout.println(line);
+				
 				if("exit".equalsIgnoreCase(line)) {
 					sock.close();
 					System.out.println("====================================================");
@@ -29,6 +32,7 @@ public class Client {
 					System.out.println("====================================================");
 					System.exit(0);
 				}
+				
 				System.out.println("Server Responds With: "+cin.readLine());
 				
 				
